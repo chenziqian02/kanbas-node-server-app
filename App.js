5dcc5f7ea94acee046bb7a601fc1ee3b899a8764
chenziqian02 <117554970+chenziqian02@users.noreply.github.com>
2024-04-05 00:53:48 -0400

    finished assignments

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import express from "express";
+import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
+
 
 import cors from "cors";
 
@@ -10,5 +13,7 @@ app.use(cors());
 app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 Lab5(app);
+Hello(app);
 app.listen(4000);
