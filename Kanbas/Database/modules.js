16f6df2bc4e8e082877824370fc6c38659ff8baf
chenziqian02 <117554970+chenziqian02@users.noreply.github.com>
2024-04-04 22:33:04 -0400

    first commit

---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,209 @@
+export default [
+    {
+        _id: "M001",
+        name: "Resources",
+        description: "Introduction to the resources",
+        course: "CS101",
+        lessons: [
+            {
+                _id: "L101",
+                name: "Syllabus",
+                description: "Syllabus.",
+                module: "M001"
+            },
+            {
+                _id: "L102",
+                name: "Office Hours",
+                description: "OH of the course",
+                module: "M001"
+            }
+        ]
+    },
+    {
+        _id: "M002",
+        name: "Required Textbook",
+        description: "Introduction to the course",
+        course: "CS101",
+        lessons: [
+            {
+                _id: "L101",
+                name: "Required Text - Full Stack Developer",
+                description: "Required Text - Full Stack Developer.",
+                module: "M002"
+            },
+            {
+                _id: "L102",
+                name: "Customer Service Portal",
+                description: "Customer Service Portal.",
+                module: "M002"
+            }
+        ]
+    },
+    {
+        _id: "M003",
+        name: "Week 0 - Intro",
+        description: "Introduction to the course",
+        course: "CS101",
+        lessons: [
+            {
+                _id: "L101",
+                name: "Introduction to the course",
+                description: "A brief history of web development.",
+                module: "M003"
+            },
+            {
+                _id: "L102",
+                name: "Learn what is Web Development",
+                description: "Basic principles of web development.",
+                module: "M003"
+            },
+            {
+                _id: "L103",
+                name: "Creating a development environment",
+                description: "Overview of different types of web languages.",
+                module: "M003"
+            }
+        ]
+    },
+    {
+        _id: "M004",
+        name: " Week 1 - HTML",
+        description: "Learn how to create user interfaces with HTML.",
+        course: "CS101",
+        lessons: [
+            {
+                _id: "L201",
+                name: "Learn how to create user interfaces with HTML",
+                description: "Full Stack Developer - Creating User Interfaces With HTML.",
+                module: "M004"
+            },
+            {
+                _id: "L202",
+                name: "Keep working on assignment 1",
+                description: "Complete assignment 1.",
+                module: "M004"
+            },
+            {
+                _id: "L203",
+                name: "Deploy the assignment to Netlify",
+                description: "Understanding netlify.",
+                module: "M004"
+            }
+        ]
+    },
+    {
+        _id: "M101",
+        name: "Introduction to Rocket Propulsion",
+        description: "Basic principles of rocket propulsion and rocket engines.",
+        course: "RS101",
+        lessons: [
+            {
+                _id: "L101",
+                name: "History of Rocketry",
+                description: "A brief history of rocketry and space exploration.",
+                module: "M101"
+            },
+            {
+                _id: "L102",
+                name: "Rocket Propulsion Fundamentals",
+                description: "Basic principles of rocket propulsion.",
+                module: "M101"
+            },
+            {
+                _id: "L103",
+                name: "Rocket Engine Types",
+                description: "Overview of different types of rocket engines.",
+                module: "M101"
+            }
+        ]
+    },
+    {
+        _id: "M102",
+        name: "Fuel and Combustion",
+        description: "Understanding rocket fuel, combustion processes, and efficiency.",
+        course: "RS101",
+        lessons: [
+            {
+                _id: "L201",
+                name: "Rocket Fuel",
+                description: "Overview of different types of rocket fuels.",
+                module: "M102"
+            },
+            {
+                _id: "L202",
+                name: "Combustion Processes",
+                description: "Understanding combustion processes and efficiency.",
+                module: "M102"
+            },
+            {
+                _id: "L203",
+                name: "Combustion Instability",
+                description: "Understanding combustion instability and mitigation.",
+                module: "M102"
+            }
+        ]
+    },
+    {
+        _id: "M103",
+        name: "Nozzle Design",
+        description: "Principles of rocket nozzle design and performance optimization.",
+        course: "RS101",
+        lessons: [
+            {
+                _id: "L301",
+                name: "Nozzle Design",
+                description: "Overview of different types of rocket nozzles.",
+                module: "M103"
+            },
+            {
+                _id: "L302",
+                name: "Nozzle Performance",
+                description: "Understanding nozzle performance and efficiency.",
+                module: "M103"
+            },
+            {
+                _id: "L303",
+                name: "Nozzle Optimization",
+                description: "Optimizing nozzle design for specific applications.",
+                module: "M103"
+            }
+        ]
+    },
+    {
+        _id: "M201",
+        name: "Fundamentals of Aerodynamics",
+        description: "Basic aerodynamic concepts and fluid dynamics principles.",
+        course: "RS102",
+    },
+    {
+        _id: "M202",
+        name: "Subsonic and Supersonic Flow",
+        description: "Understanding subsonic and supersonic aerodynamic behaviors.",
+        course: "RS102",
+    },
+    {
+        _id: "M203",
+        name: "Aerodynamic Heating",
+        description: "Study of aerodynamic heating and thermal protection systems.",
+        course: "RS102",
+    },
+    {
+        _id: "M301",
+        name: "Spacecraft Structural Design",
+        description:
+            "Fundamentals of designing spacecraft structures and materials selection.",
+        course: "RS103",
+    },
+    {
+        _id: "M302",
+        name: "Orbital Mechanics",
+        description: "Understanding orbital dynamics and mission planning.",
+        course: "RS103",
+    },
+    {
+        _id: "M303",
+        name: "Spacecraft Systems Engineering",
+        description: "Overview of spacecraft systems and subsystems engineering.",
+        course: "RS103",
+    }
+]
