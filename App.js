167c35a4089a9434ce9ac92aadee462b8203ae78
chenziqian02 <117554970+chenziqian02@users.noreply.github.com>
2024-04-18 16:15:23 -0400

    first commit - signin functionality completed

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
-
-
 import cors from "cors";
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
